32299b4561c4f556f34ac3e8ae3e9edd7f25d6cc
Wasllot dev <38118800+wasllot@users.noreply.github.com>
2019-01-13 11:39:14 -0400

    Update authenticated.js

--- middlewares/authenticated.js
+++ middlewares/authenticated.js
@@ -3,7 +3,6 @@
 var jwt = require('jwt-simple');
 var moment = require('moment');
 
-// string secreto que sólo lo conoce el programador del backend
 var secret = 'esta_es_mi_clave_secreta_de_mi_curso_de_red_social'; 
 
 exports.ensureAuth = function(req, res, next){
@@ -14,19 +13,19 @@ exports.ensureAuth = function(req, res, next){
 	
 	var token = req.headers.authorization.replace(/['"]+/g, ''); 
 
-	// decodificamos el payload
+	// payload decode
 	try{
 		var payload = jwt.decode(token, secret);
 		if(payload.exp <= moment().unix()){
-			return res.status(401).send({message: 'El token ha expirado!!'});
+			return res.status(401).send({message: 'Token has expired'});
 		}
 	}catch(ex){
-		return res.status(404).send({message: 'El token no es válido!!'});
+		return res.status(404).send({message: 'Invalid token'});
 	}
 
-	// guardo los datos del usuario
+
 	req.user = payload;
 
-	// saltamos a lo siguiente
+	
 	next();
 }
